e78bdec6b482a41c6bbc48d8bab62134bc7fcd08
Arda Cansiz <arda.cansiz@gmail.com>
2023-04-19 23:09:52 +0200

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { useRoutes } from 'react-router-dom'
+
 // import './App.css';
 
+import Home from './pages/Home'
+import PageWrapper from './components/PageWrapper'
+
 function App() {
-  return (
-    <>
-    </>
-  );
+  return useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '*', element: <PageWrapper /> },
+  ])
 }
 
 export default App;
