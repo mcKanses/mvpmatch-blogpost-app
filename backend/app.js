8e855e21413582a8579101fbb3b53cd3dea3c6d8
Arda Cansiz <arda.cansiz@gmail.com>
2023-04-20 22:53:22 +0200

    Apply cors middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 
 import mainRouter from './routers/main.router.js'
 
 const PORT = 8000;
 
 const app = express()
+
+app.use(cors)
 
 app.use(mainRouter)
 
